60b51d1283f314acefa06a14579cd8aab35b76af
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-11-29 01:48:07 +0800

    To be finished: Implementation of network

--- scripts/LSTMNetwork.py
+++ scripts/LSTMNetwork.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 from ModelInterface import ModelInterface
 from tensorflow.contrib import rnn
 
-class LSTMNetwork(ModelInterface):
+class SingleLayerLSTMNetwork(ModelInterface):
 		timesteps 		= None
 		num_input 		= None
 		num_output 		= None
 		hidden_units 	= None
 		loss 			= None
 		optimizer 		= None
+		dropout 		= None
 		learning_rate 	= None
 
 		def __init__(self):
@@ -42,12 +43,26 @@ class LSTMNetwork(ModelInterface):
 			self.learning_rate = learning_rate
 			return self
 
+		def setDropout(self, dropout):
+			self.dropout = dropout
+			return self
+
 		def build(self):
-			pass
+			network = rnn.LSTMCell(self.hidden_units)
+			if(self.dropout is not None):
+				network = rnn.DropoutWrapper(network, output_keep_prob=self.dropout)
+
+			input_tensor 	= tf.placeholder(tf.float32, [None, None, self.num_input])
+			output, _ 	 	= tf.nn.dynamic_rnn(network, input_tensor, dtype=tf.float32)
+			last_output  	= tf.transpose(output, [1,0,2])
+			last 			= tf.gather(last_output, int(last_output.get_shape()[0]) - 1)
+			import IPython
+			IPython.embed()
 
 		def getModel(self):
 			pass
 
 if __name__ == "__main__":
-	lstm = LSTMNetwork()
+	lstm = SingleLayerLSTMNetwork()
+	lstm.setHiddenUnits(100).setInputSize(160)
 	lstm.build()
