786bb0faa47d7b44b82cd7cb55a347a90d6c3835
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-11-28 17:04:35 +0800

    Modifications to LSTMNetwork

--- scripts/LSTMNetwork.py
+++ scripts/LSTMNetwork.py
@@ -1,27 +1,29 @@
 import tensorflow as tf 
 from ModelInterface import ModelInterface
+from tensorflow.contrib import rnn
 
 class LSTMNetwork(ModelInterface):
-		seq_units 		= None
-		input_size 		= None
-		output_size 	= None
+		timesteps 		= None
+		num_input 		= None
+		num_output 		= None
 		hidden_units 	= None
 		loss 			= None
 		optimizer 		= None
+		learning_rate 	= None
 
 		def __init__(self):
 			pass
 
-		def setNoOfUnits(self, no_seq_units):
-			self.seq_units = no_seq_units
+		def setTimeSteps(self, timesteps):
+			self.timesteps = timesteps
 			return self
 
 		def setInputSize(self, input_size):
-			self.input_size = input_size
+			self.num_input = input_size
 			return self
 
 		def setOutputSize(self, output_size):
-			self.output_size = output_size
+			self.num_output = output_size
 			return self
 
 		def setHiddenUnits(self, no_hidden_units):
@@ -34,6 +36,10 @@ class LSTMNetwork(ModelInterface):
 
 		def setOptimizer(self, optimizer):
 			self.optimizer = optimizer
+			return self
+
+		def setLearningRate(self, learning_rate):
+			self.learning_rate = learning_rate
 			return self
 
 		def build(self):
