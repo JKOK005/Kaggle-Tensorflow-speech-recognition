a12c20a1afc5513ec0f251bd52930fda9d429379
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-11-29 12:53:02 +0800

    Implementation of LSTM network without testing
    - Implemented build method to create single LSTM
    - Implemented method to add loss function to graph
    - Implemented method to add optimizer to graph
    
    TODO:
    - Change loss and optimizer handling to COR design pattern

--- scripts/LSTMNetwork.py
+++ scripts/LSTMNetwork.py
@@ -7,10 +7,14 @@ class SingleLayerLSTMNetwork(ModelInterface):
 		num_input 		= None
 		num_output 		= None
 		hidden_units 	= None
-		loss 			= None
+		cost 			= None
 		optimizer 		= None
 		dropout 		= None
 		learning_rate 	= None
+
+		__prediction 	= None
+		__objective 	= None
+		__error 		= None
 
 		def __init__(self):
 			pass
@@ -31,8 +35,8 @@ class SingleLayerLSTMNetwork(ModelInterface):
 			self.hidden_units = no_hidden_units
 			return self
 
-		def setLossFunction(self, loss_fn):
-			self.loss = loss_fn
+		def setLossFunction(self, loss_type):
+			self.cost = loss_type
 			return self
 
 		def setOptimizer(self, optimizer):
@@ -47,22 +51,48 @@ class SingleLayerLSTMNetwork(ModelInterface):
 			self.dropout = dropout
 			return self
 
+		def __getLoss(self, loss_type):
+			loss = None
+			output_tensor = tf.placeholder(tf.float32, [None, self.num_output]) 
+
+			if(loss_type == 'cross_entropy'):
+				loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.__prediction, labels=output_tensor))
+			return loss
+
+		def __getOptimizer(self, optimizer_type, learning_rate):
+			optimizer = None
+			if(optimizer_type == 'gradient_descent'):
+				optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+			return optimizer
+
+		def __setObjective(self):
+			loss = self.__getLoss(self.cost)
+			optimizer = self.__getOptimizer(self.optimizer, self.learning_rate)
+			self.__objective = optimizer.minimize(loss)
+			return
+
 		def build(self):
 			network = rnn.LSTMCell(self.hidden_units)
 			if(self.dropout is not None):
 				network = rnn.DropoutWrapper(network, output_keep_prob=self.dropout)
 
-			input_tensor 	= tf.placeholder(tf.float32, [None, None, self.num_input])
-			output, _ 	 	= tf.nn.dynamic_rnn(network, input_tensor, dtype=tf.float32)
-			last_output  	= tf.transpose(output, [1,0,2])
-			last 			= tf.gather(last_output, int(last_output.get_shape()[0]) - 1)
-			import IPython
-			IPython.embed()
+			input_tensor 	= tf.placeholder(tf.float32, [None, self.timesteps, self.num_input])
 
+			output, _ 	 	= tf.nn.dynamic_rnn(network, input_tensor, dtype=tf.float32)
+			transpose_time_axis = tf.transpose(output, [1,0,2])
+			last_timestamp 	= tf.gather(transpose_time_axis, int(transpose_time_axis.get_shape()[0]) - 1)
+
+			init_weight 	= tf.Variable(tf.random_normal([self.hidden_units, self.num_output]))
+			init_bias 		= tf.Variable(tf.random_normal([self.num_output]))
+
+			self.__prediction = tf.nn.softmax(tf.matmul(last_timestamp, init_weight) + init_bias)
+			self.__setObjective()
+			
 		def getModel(self):
 			pass
 
 if __name__ == "__main__":
 	lstm = SingleLayerLSTMNetwork()
-	lstm.setHiddenUnits(100).setInputSize(160)
-	lstm.build()
+	lstm.setHiddenUnits(100).setInputSize(160).setOutputSize(12).setTimeSteps(40) \
+		.setLossFunction('cross_entropy').setOptimizer('gradient_descent').setLearningRate(0.01) \
+		.build()
